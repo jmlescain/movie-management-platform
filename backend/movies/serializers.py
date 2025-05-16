d232d6d4d0fd061916587ae546a0c7cab4d0c8da
Jan Lescain <jmlescain@gmail.com>
2025-05-16 14:44:10 +0800

    (backend) Add id field to response payload

--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Movie
-        fields = ["title", "description", "date_added", "video_file"]
+        fields = ["id", "title", "description", "date_added", "video_file"]
