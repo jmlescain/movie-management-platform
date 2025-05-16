d632392b4e55bb4ddd655a1b4d57f02741c5b43f
Jan Lescain <jmlescain@gmail.com>
2025-05-16 10:16:11 +0800

    (backend) Create movies model

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'uploads/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-class Movies(models.Model):
+import uuid
+
+def generate_filename(instance, filename):
+    return "uploads/{0}_{1}".format(uuid.uuid4(), filename)
+
+class Movie(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     date_added = models.DateField("date added")
-    video_file = models.FileField(upload_to="uploads/")
+    video_file = models.FileField(upload_to=generate_filename)
