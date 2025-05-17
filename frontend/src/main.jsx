da61d90242f5d0502cf73130e5a4e0a9dbebda0a
Jan Lescain <jmlescain@gmail.com>
2025-05-17 09:00:05 +0800

    (frontend) Redirect unknown URL path to index

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 import Upload from "./Upload.jsx";
@@ -11,6 +11,7 @@ createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
       <Routes>
+        <Route path="*" element={<Navigate to="/" replace />} />
         <Route path="/" element={<App />} />
         <Route path="/upload" element={<Upload />} />
         <Route path="/details/:movieId" element={<MovieDetails />} />
