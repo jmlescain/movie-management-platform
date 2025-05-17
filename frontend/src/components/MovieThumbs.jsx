a19ee40a99386c3549b16a2285747d4953cea75f
Jan Lescain <jmlescain@gmail.com>
2025-05-17 08:59:08 +0800

    (frontend) Create movie thumbnail component

--- frontend/src/components/MovieThumbs.jsx
+++ frontend/src/components/MovieThumbs.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useNavigate } from "react-router";
+
 import genericThumbnail from "../assets/generic_play_cropped.png";
 
 export default function MovieThumbs({
@@ -8,15 +10,21 @@ export default function MovieThumbs({
   date_added,
   video_file,
 }) {
+  const navigate = useNavigate();
+
+  const onThumbnailClick = () => {
+    navigate(`/details/${id}`);
+  };
+
   return (
-    <div>
+    <div className="hover:cursor-pointer" onClick={onThumbnailClick}>
       <img
         className="rounded-md"
         src={genericThumbnail}
         width={256}
         height={144}
       />
-      <p className="text-lg font-bold mt-2">{title}</p>
+      <p className="text-lg font-bold mt-2 hover:text-neutral-500">{title}</p>
     </div>
   );
 }
