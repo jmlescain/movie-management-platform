ae3a0533ec7b3a2a10ef8bf653568eb135cdf7a9
Jan Lescain <jmlescain@gmail.com>
2025-05-17 23:22:46 +0800

    (frontend) Apply sorting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,18 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { getMovies } from "./services/api";
 import MovieThumbs from "./components/MovieThumbs";
-import Header from "./components/Header";
+import UploadButton from "./components/UploadButton";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [sorting, setSorting] = useState("title");
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchMovies() {
       try {
         setIsLoading(true);
-        const res = await getMovies();
+        const res = await getMovies(sorting);
         if (res) {
           setMovies(res);
         }
@@ -25,11 +26,26 @@ function App() {
     }
 
     fetchMovies();
-  }, []);
+  }, [sorting]);
 
   return (
     <>
-      <Header />
+      <UploadButton />
+      <div className="flex flex-row justify-end mt-4 mr-8 items-center">
+        <label htmlFor="sort-select" className="text-xl mr-4">
+          Sort by:
+        </label>
+        <select
+          className="border-2 rounded-sm w-40 text-xl"
+          name="sort-select"
+          id="sort-select"
+          value={sorting}
+          onChange={(e) => setSorting(e.target.value)}
+        >
+          <option value="title">Title</option>
+          <option value="date_added">Date Added</option>
+        </select>
+      </div>
       {movies.length === 0 && (
         <p className="flex justify-center items-center h-full w-full absolute top-0 left-0 -z-10">
           Start uploading your movies
