d397fa1a8749a6624bfcda67c71067e0d16b9f2d
Jan Lescain <jmlescain@gmail.com>
2025-05-17 09:37:52 +0800

    (backend) Auto add date

--- backend/movies/models.py
+++ backend/movies/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 import uuid
+import datetime
 
 def generate_filename(instance, filename):
     return "uploads/{0}_{1}".format(uuid.uuid4(), filename)
@@ -8,5 +9,5 @@ def generate_filename(instance, filename):
 class Movie(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
-    date_added = models.DateField("date added")
+    date_added = models.DateField("date added", auto_now_add=True)
     video_file = models.FileField(upload_to=generate_filename)
